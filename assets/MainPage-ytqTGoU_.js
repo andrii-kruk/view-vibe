a2341a4cb0d584ebf1025ad89037165b90a01c87
andrii-kruk <119102553+andrii-kruk@users.noreply.github.com>
2023-12-06 10:14:21 +0000

    Deploying to gh-pages from @ andrii-kruk/view-vibe@ef131fff912cd492b4b0d324effe7f155c931692 🚀

---
+++ assets/MainPage-ytqTGoU_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-x8k-RM5W.js";const r=()=>e.jsx("section",{children:e.jsx("h1",{children:"Main Page"})});export{r as default};
---
+++ assets/SecondPage-CuMsJ8I5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-x8k-RM5W.js";const o=()=>e.jsx("section",{children:e.jsx("h1",{children:"Second Page"})});export{o as default};

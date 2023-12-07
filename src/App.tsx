774982d2ff8d66be7d8fba0006d264fb756a8739
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-12-07 09:48:49 +0100

    fix Suspense and Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, lazy, Suspense } from "react";
+import { FC, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const HomePage = lazy(() => import("@pages/HomePage/HomePage"));
@@ -6,20 +6,18 @@ const MoviesPage = lazy(() => import("@pages/MoviesPage/MoviesPage"));
 const SeriesPage = lazy(() => import("@pages/SeriesPage/SeriesPage"));
 const BookmarkedPage = lazy(() => import("@pages/BookmarkedPage/BookmarkedPage"));
 
-import { Layout, Loader } from "@components/index";
+import { Layout } from "@components/index";
 
 const App: FC = () => {
   return (
-    <Suspense fallback={<Loader color="#36d7b7" />}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviesPage />} />
-          <Route path="series" element={<SeriesPage />} />
-          <Route path="bookmarked" element={<BookmarkedPage />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<MoviesPage />} />
+        <Route path="series" element={<SeriesPage />} />
+        <Route path="bookmarked" element={<BookmarkedPage />} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,17 @@
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Container, Sidebar } from "@components/index";
+import { Container, Loader, Sidebar } from "@components/index";
 
 const Layout: FC = () => {
   return (
     <>
       <Sidebar />
       <main>
-        <Container>
+      <Suspense fallback={<Loader color="#36d7b7" />}>
+       <Container>
           <Outlet />
         </Container>
+       </Suspense>
       </main>
     </>
   );

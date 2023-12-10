f47a0c838f8d3a83acb67292b6dfb36f478b7cf5
andrii-kruk <119102553+andrii-kruk@users.noreply.github.com>
2023-12-10 17:49:51 +0000

    Deploying to gh-pages from @ andrii-kruk/view-vibe@2c8ede6789fa3fdbce8148ce46cf3ab2bc960d7c 🚀

---
+++ assets/BookmarkedPage-sUnIJtcb.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-caXVfWD9.js";const s=()=>e.jsx(e.Fragment,{children:e.jsxs("section",{children:[e.jsx("h2",{children:"Bookmarked Movies"}),e.jsx("ul",{})]})});export{s as default};
